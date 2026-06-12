8aeccea837d22c30f9a8a2cc9d8657ce05a52ac8
Shashank Singh <singhshashank875@gmail.com>
2026-06-12 20:00:56 +0530

    feat: integrate Vercel Analytics and add rewrites configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,11 @@
 import Navbar from "./components/Navbar";
 import { AppRouter } from "./router/AppRouter";
-import { Analytics } from "@vercel/analytics/next";
 
 export default function App() {
   return (
     <>
       <Navbar />
       <AppRouter />
-      <Analytics />
     </>
   );
 }
