a55eb3c6ab2d75ad2e26fa0974b96cdd7a0d91af
Shashank Singh <singhshashank875@gmail.com>
2026-06-12 16:33:59 +0530

    feat: add Analytics component to App for enhanced tracking

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import { AppRouter } from "./router/AppRouter";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App() {
   return (
     <>
       <Navbar />
       <AppRouter />
+      <Analytics />
     </>
   );
 }
